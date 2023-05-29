42847adfb478aa3f13cf2dfb3d6c038daa4e5942
Debdeep1989 <134961387+Debdeep1989@users.noreply.github.com>
2023-05-29 23:18:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 Breakfast Menu
-Omega3 & Blueberry oatmeal 
+Omega 3 & Blueberry oatmeal 
 Kale, Spinach and Rocket Smoothie
 Hard-Boiled Free-Range Egg
